3e598094ee8649bb2ac6dc7dbc0c380a9dc94ae2
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-08 15:11:02 +0330

    in to mainwindow.xaml add  border grid.column=0

--- WPFProductManagment/obj/Debug/net8.0-windows/WPFProductManagment.AssemblyInfo.cs
+++ WPFProductManagment/obj/Debug/net8.0-windows/WPFProductManagment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPFProductManagment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69a28577415795a8e866ffb3be8cd74c7492d1dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feab41b1da3b1f752c36839e2f4927c36180d16e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPFProductManagment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPFProductManagment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
