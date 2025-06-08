def291b37c9855c268a2c4ef97a2a6153841ecd5
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-08 15:31:38 +0330

    in to mainwindow.xaml add  border grid.column=1 full and run

--- WPFProductManagment/obj/Debug/net8.0-windows/WPFProductManagment.AssemblyInfo.cs
+++ WPFProductManagment/obj/Debug/net8.0-windows/WPFProductManagment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPFProductManagment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e598094ee8649bb2ac6dc7dbc0c380a9dc94ae2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98144e79dc72ea18308ba684197b1f82932ac24a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPFProductManagment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPFProductManagment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
