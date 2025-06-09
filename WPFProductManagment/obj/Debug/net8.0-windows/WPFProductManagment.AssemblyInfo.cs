f0661cfe34039a5dc1d1f01fd5674429556f94bf
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-09 15:00:55 +0330

    add FontFamily= to all label and run

--- WPFProductManagment/obj/Debug/net8.0-windows/WPFProductManagment.AssemblyInfo.cs
+++ WPFProductManagment/obj/Debug/net8.0-windows/WPFProductManagment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPFProductManagment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+def291b37c9855c268a2c4ef97a2a6153841ecd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a02d5a0b59ffb10170d73161b59c2118be1c1d51")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPFProductManagment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPFProductManagment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
