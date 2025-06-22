4f15dceed029c2abf933d3c5f2ba9faad9357d24
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-22 22:46:27 +0330

    fix name product in DataAccess -2

---
+++ DataAccess/Models/CustomerDataAccess.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class CustomerDataAccess
+    {
+        public List<Customer> Customers { get; set; } = new List<Customer>();
+
+        public CustomerDataAccess()
+        {
+            ReadCustomer();
+        }
+
+        private void ReadCustomer()
+        {
+            Customer cst1 = new Customer()
+            {
+                Id = 1,
+                FirstName = "mamad",
+                LastName = "zmani",
+                PhoneNumber = 0913131441,
+                Address = "esf",
+               
+            };
+            Customer cst2 = new Customer()
+            {
+                Id = 2,
+                FirstName = "saiid",
+                LastName = "salimian",
+                PhoneNumber = 09136454383,
+                Address = "esf",
+             
+            };
+            Customers.Add(cst1);
+            Customers.Add(cst2);
+        }
+
+        public void AddCustomer(Customer cst)
+        {
+            Customers.Add(cst);
+        }
+
+        public void RemoveCustomer(int id)
+        {
+            Customer temp = Customers.First(x => x.Id == id);
+
+            Customers.Remove(temp);
+        }
+
+        public void EditCustomer(Customer cst)
+        {
+            Customer temp = Customers.First(x => x.Id == cst.Id);
+
+            int index = Customers.IndexOf(temp);
+
+            Customers[index] = cst;
+        }
+
+        public int getNextId()
+        {
+            int index = Customers.Any() ? Customers.Max(x => x.Id) + 1 : 1;
+            return index;
+        }
+
+    }
+}
---
+++ DataAccess/Models/EmployeeDataAccess.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Models;
+
+namespace DataAccess.Models
+{
+    public class EmployeeDataAccess
+    {
+
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+
+        public EmployeeDataAccess()
+        {
+            ReadEmployee();
+        }
+
+        private void ReadEmployee()
+        {
+            Employee emp1 = new Employee()
+            {
+                Id = 1,
+                FirstName = "bahrouz",
+                LastName = "ashrafzade",
+                PhoneNumber = 0913131441,
+                Address = "esf",
+                Department = Department.Production,
+                BaseSalary = 1500
+            };
+            Employee emp2 = new Employee()
+            {
+                Id = 2,
+                FirstName = "davoood",
+                LastName = "ashrafzade",
+                PhoneNumber = 09136454383,
+                Address = "esf",
+                Department = Department.Production,
+                BaseSalary = 2500
+            };
+            Employees.Add(emp1);
+            Employees.Add(emp2);
+        }
+
+        public void AddEmployee(Employee emp)
+        {
+            Employees.Add(emp);
+        }
+
+        public void RemoveEmployee(int id)
+        {
+            Employee temp = Employees.First(x => x.Id == id);
+
+            Employees.Remove(temp);
+        }
+
+        public void EditEmployee(Employee emp)
+        {
+            Employee temp = Employees.First(x => x.Id == emp.Id);
+
+            int index = Employees.IndexOf(temp);
+
+            Employees[index] = emp;
+        }
+
+        public int getNextId()
+        {
+            int index = Employees.Any() ? Employees.Max(x => x.Id) + 1 : 1;
+            return index;
+        }
+
+
+
+
+    }
+}
