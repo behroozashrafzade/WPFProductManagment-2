234863ffd62013aa6e7f7bb3a9d0c1727c318c25
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-23 16:54:02 +0330

    add  obs in employedataaccese

--- DataAccess/EmployeeDataAccess.cs
+++ DataAccess/EmployeeDataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace DataAccess
     public class EmployeeDataAccess
     {
 
-        public List<Employee> Employees { get; set; } = new List<Employee>();
+        public ObservableCollection<Employee> Employees { get; set; } = new ObservableCollection<Employee>();
 
         public EmployeeDataAccess()
         {
