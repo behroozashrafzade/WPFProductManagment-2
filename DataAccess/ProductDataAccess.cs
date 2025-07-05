c4ae865f73748439c22b385ccde2ed3217c2f0a7
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-07-05 16:18:57 +0330

    add  database update for file .csv in ProductDataAccess and Run

--- DataAccess/ProductDataAccess.cs
+++ DataAccess/ProductDataAccess.cs
@@ -10,6 +10,7 @@ namespace DataAccess
 {
     public class ProductDataAccess
     {
+        private string path = @"./DemoDBProduct.csv";
         public ObservableCollection<Product> Products { get; set; } = new ObservableCollection<Product>();
 
         public ProductDataAccess()
@@ -19,29 +20,56 @@ namespace DataAccess
 
         private void ReadProducts()
         {
-            Product pr1 = new Product()
+            using (var reader = new StreamReader(path))
             {
-                Id = 1,
-                Name = "laptop",
-                Author = "parviz ashrafzade",
-                AvailableCount = 12,
-                Price = 17
-            };
-            Product pr2 = new Product()
-            {
-                Id = 2,
-                Name = "xbox",
-                Author = "parviz ashrafzade",
-                AvailableCount = 5,
-                Price = 34
-            };
-            Products.Add(pr1);
-            Products.Add(pr2);
+                Products.Clear();
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    string[] values = line.Split(';');
+                   
+                    Product emp = new Product()
+                    {
+                        Id = Convert.ToInt32(values[0]),
+                        Name = values[1],
+                        Author = values[2],
+                        Price = Convert.ToUInt64(values[3]),
+                        AvailableCount = Convert.ToInt32(values[4])
+
+                    };
+                    Products.Add(emp);
+                }
+            }
         }
 
-        public void AddProduct(Product product)
+        private void SaveProducts()
+        {
+            using (var writer = new StreamWriter(path))
+            {
+                foreach (Product emp in Products)
+                {
+
+                    string Id = emp.Id.ToString();
+                    string Name = emp.Name;
+                    string Author = emp.Author;
+                    string Price = emp.Price.ToString();
+                    string AvailableCountl = emp.AvailableCount.ToString();
+                    
+
+                    string line = string.Format("{0};{1};{2};{3};{4}"
+                        , Id, Name, Author, Price, AvailableCountl);
+                    writer.WriteLine(line);
+                }
+            }
+        }
+    
+
+
+public void AddProduct(Product product)
         {
             Products.Add(product);
+            SaveProducts();
         }
 
         public void RemoveProduct(int id)
@@ -49,6 +77,7 @@ namespace DataAccess
             Product temp = Products.First(x => x.Id == id);
 
             Products.Remove(temp);
+            SaveProducts();
         }
 
         public void EditProduct(Product product)
--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be2f765256990ef5ecbb16fe76e4a3cb1e4708e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e1f0af3c252dab171af77efcfe5ccbdbcc896e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
